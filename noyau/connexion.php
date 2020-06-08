6460a443655a1dabae880f751369f1e561ddaec0
Morgan <gann.gann87@gmail.com>
2020-06-08 14:25:52 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ noyau/connexion.php
@@ -0,0 +1,19 @@
+<?php
+
+// hote de la base de donnée
+define('PASSWORD','root';)
+$dbHost = '';
+
+// Nom de la base de donnée
+define('PASSWORD','root';)
+
+$dbNam = '';
+
+// login de la base de donnée
+define('PASSWORD','root';)
+
+$dbLogin = '';
+
+// Password de la base de donnée
+define('PASSWORD','root';)
+$dbPass = 'root';
